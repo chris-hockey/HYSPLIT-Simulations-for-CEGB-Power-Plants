832a103c7dc36d154a1a752c336b8634c2a877f6
chris-hockey <christopherhockey@icloud.com>
2026-08-24 02:18:28 +0100

    cleared rouge comment

--- code/data_processing/power_plants/1_pp_cleaning.py
+++ code/data_processing/power_plants/1_pp_cleaning.py
@@ -188,4 +188,3 @@ log.info(
 # ==============================================================================
 out_path = OUT_DIR / "clean_cegb.csv"
 cfpp_trimmed.to_csv(out_path, index=False)
-# GIT_TEST
